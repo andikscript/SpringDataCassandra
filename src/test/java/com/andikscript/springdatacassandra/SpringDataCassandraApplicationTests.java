cf515f64bc64c9abdfbea456629c1441664e213c
andik.saputro <andik.saputro@pactindo.com>
2022-09-28 15:18:20 +0700

    intiial commit

---
+++ src/test/java/com/andikscript/springdatacassandra/SpringDataCassandraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andikscript.springdatacassandra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataCassandraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
