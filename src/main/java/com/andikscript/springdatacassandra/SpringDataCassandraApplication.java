6378391b9256fc20fb3bdc4f406e9b4262a88651
andik.saputro <andik.saputro@pactindo.com>
2022-09-29 10:28:05 +0700

    add enable cassandra repository

--- src/main/java/com/andikscript/springdatacassandra/SpringDataCassandraApplication.java
+++ src/main/java/com/andikscript/springdatacassandra/SpringDataCassandraApplication.java
@@ -2,8 +2,10 @@ package com.andikscript.springdatacassandra;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
 @SpringBootApplication
+@EnableCassandraRepositories
 public class SpringDataCassandraApplication {
 
     public static void main(String[] args) {
